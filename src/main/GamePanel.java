43377172b7906fab599e7c2c3fef8b4f1e961957
ryan <ryanliu25@gmail.com>
2025-05-16 18:07:06 -0700

    v 0.1.0 prerelease 2
    changes:
    added keyHandler
    made rectangle movable - testing for player movement
    made rectangle move at the right speed despite the variable fps
    added variable fps
    added fps counter
    saved memory by disposing

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class GamePanel extends JPanel implements Runnable{
     int scale = 3;
@@ -10,32 +12,93 @@ public class GamePanel extends JPanel implements Runnable{
     int screenWidth = maxWidth * scale;
     int screenHeight = maxHeight * scale;
 
+    String targetFPS = "unlimited";
+    float FPS = 0;
+
+    long currentTime;
+    double delta = 0;
+    long lastTime = System.nanoTime();
+    double refreshTime = 0;
+    double FPSInterval;
+    ArrayList<Long> frames = new ArrayList<>();
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+        double playerX = 100;
+        double playerY = 100;
+        double playerSpeed = 100;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); // buffer for better performance
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
 
     }
     public void startGameThread() {
         gameThread = new Thread(this);
         gameThread.start();
     }
+
+    public void calculateFPS() {
+        for (int i = frames.size() - 1; i >= 0; i--) {
+            if (currentTime - frames.get(i) >= 1000000000) {
+                frames.remove(i);
+            }
+        }
+        FPS = frames.size();
+    }
     @Override
     public void run() {
         while(gameThread != null) {
-            update();
-            repaint();//call paintComponent
+            if (Objects.equals(targetFPS, "unlimited")) {
+                currentTime = System.nanoTime();
+                refreshTime = (double)(currentTime - lastTime) / 1000000000;
+                lastTime = currentTime;
+                frames.add(currentTime);
+                calculateFPS();
+                update();
+                repaint();
+            } else {
+                FPSInterval = 1000000000 / Double.parseDouble(targetFPS);
+                currentTime = System.nanoTime();
+
+                delta += (currentTime - lastTime) / FPSInterval;
+                lastTime = currentTime;
+                if (delta >= 1) {
+                    frames.add(currentTime);
+                    calculateFPS();
+                    update();
+                    repaint();
+                    delta--;
+                }
+
+            }
+
         }
     }
     public void update() {
-        System.out.println("updated"); //placeholder for update
+        if (keyH.wPressed) {
+            playerY -= playerSpeed * refreshTime;
+        }
+        if (keyH.sPressed) {
+            playerY += playerSpeed * refreshTime;
+        }
+        if (keyH.aPressed) {
+            playerX -= playerSpeed * refreshTime;
+        }
+        if (keyH.dPressed) {
+            playerX += playerSpeed * refreshTime;
+        }
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
         g2.setColor(Color.white);
-        g2.fillRect(10, 20, 40, 40);
+        g2.fillRect((int)Math.round(playerX), (int)Math.round(playerY), 40, 40);
+        g2.drawString(String.valueOf(FPS), 10, 10);
+        g2.dispose();
     }
 }
